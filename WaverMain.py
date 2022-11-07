ff1542cf159a538e387af934bb49d659cda871d6
Background <76162743+YaIiya@users.noreply.github.com>
2022-11-07 20:29:04 +1000

    Basic Files
    Those are bsic files in this thing. Probably their contents will be sorted.

---
+++ WaverMain.py
@@ -0,0 +1,27 @@
+from vocabulary import *
+t = True
+f = False
+
+
+def exit():
+    print("Bro you sure? [Y/N]")
+    while t:
+        isExit = ""
+        isExit = input()
+        for i in range(0, 11):
+            if isExit == yes[i]:
+                print("Ok bye.")
+                quit()
+            i += 1
+        else:
+            print("Invalid. Valid values: Y/N/0/1")
+
+
+def waitSelection(sel):
+    if sel == 1:
+        print("What waveforms would you like to combine?")
+
+
+
+
+
